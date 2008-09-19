bc27145778f4a31ac4f00ac0358caabd363801f3
Josh <machomagna@gmail.com>
2008-09-19 13:51:44 -0500

    add restful authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :expenses
+
+  map.resources :deposits
+
+  map.resources :buckets
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
