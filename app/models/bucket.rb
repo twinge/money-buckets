d54df70434c1dcd1af3eaa8a8853737876b8f512
Josh Starcher <josh.starcher@gmail.com>
2009-02-10 08:30:30 -0600

    auto do expenses as well as deposits

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -11,33 +11,36 @@ class Bucket < ActiveRecord::Base
     day_of_week = today.wday + 1
     weekly = Bucket.find(:all, :conditions => {:auto_period => 'week', :auto_day => day_of_week})
     weekly.each do |bucket|
-      if !bucket.auto_amount.nil? && bucket.auto_amount > 0
-        bucket.deposits << Deposit.new(:amount => bucket.auto_amount)
-        bucket.save(false)
-      end
+      process_amount(bucket)
     end
     # Monthly
     day_of_month = today.mday
     day_of_month = 'last' if today == today.end_of_month
     monthly = Bucket.find(:all, :conditions => {:auto_period => 'month', :auto_day => day_of_month})
     monthly.each do |bucket|
-      if !bucket.auto_amount.nil? && bucket.auto_amount > 0
-        bucket.deposits << Deposit.new(:amount => bucket.auto_amount)
-        bucket.save(false)
-      end
+      process_amount(bucket)
     end
     # Yearly
     day_of_year = today.yday
     yearly = Bucket.find(:all, :conditions => {:auto_period => 'year', :auto_day => day_of_year})
     yearly.each do |bucket|
-      if !bucket.auto_amount.nil? && bucket.auto_amount > 0
-        bucket.deposits << Deposit.new(:amount => bucket.auto_amount)
-        bucket.save(false)
-      end
+      process_amount(bucket)
     end
   end
   
   def balance
     transactions.sum(:amount)
   end
+  
+  private 
+    def process_amount(bucket)
+      if !bucket.auto_amount.nil? 
+        if bucket.auto_amount > 0
+          bucket.deposits << Deposit.new(:amount => bucket.auto_amount)
+        else
+          bucket.expenses << Expense.new(:amount => bucket.auto_amount, :description => 'Auto-monthly')
+        end
+        bucket.save(false)
+      end
+    end
 end
